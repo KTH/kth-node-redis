b2e2307c1cbf0cee83565e6f8871816f1c56d02d
Patric Jansson <paddy@kth.se>
2019-08-01 13:15:54 +0200

    Removed verbose debug

--- index.js
+++ index.js
@@ -83,7 +83,6 @@ function _createClient (name, options, callback) {
 
   client.on('ready', function () {
     log.debug('Redis client ready: ' + name)
-    log.debug({ config: config }, 'Redis client config')
     log.debug(`Redis server version: ${safeGet(() => client.server_info.redis_version)}`)
     isReady = true
     callback(null, client)
