a4af699bf50866a75fa2e32438893a3208d6e863
Patric Jansson <paddy@kth.se>
2019-09-03 18:04:53 +0200

    prettier and log message update

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-'use strict'
+"use strict";
 
-const logger = require('kth-node-log')
-const redis = require('redis')
-const Promise = require('bluebird')
-const deepAssign = require('deep-assign')
-const { safeGet } = require('safe-utils')
+const logger = require("kth-node-log");
+const redis = require("redis");
+const Promise = require("bluebird");
+const deepAssign = require("deep-assign");
+const { safeGet } = require("safe-utils");
 
-Promise.promisifyAll(redis.RedisClient.prototype)
-Promise.promisifyAll(redis.Multi.prototype)
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
 
 const _defaults = {
-  retry_strategy: function (options) {
-    if (safeGet(() => options.error.code === 'ECONNREFUSED')) {
-      return new Error('Connection refused by server.')
+  retry_strategy: function(options) {
+    if (safeGet(() => options.error.code === "ECONNREFUSED")) {
+      return new Error("Connection refused by server.");
     }
 
     if (safeGet(() => options.total_retry_time > 1000 * 60 * 60)) {
-      return new Error('Retry time exhausted')
+      return new Error("Retry time exhausted");
     }
 
     if (safeGet(() => options.times_connected > 10)) {
-      return undefined
+      return undefined;
     }
 
-    return Math.max(options.attempt * 100, 3000)
+    return Math.max(options.attempt * 100, 3000);
   },
   connect_timeout: 10000 // for initially connecting to redis. This prevents hanging on a bad REDIS_URI (increased to 10s due to Azure)
-}
+};
 
-const _defaultName = 'default'
+const _defaultName = "default";
 
-let _clients = {}
+let _clients = {};
 
-function _once (fn) {
-  let called = false
-  let value = false
+function _once(fn) {
+  let called = false;
+  let value = false;
 
-  return function () {
+  return function() {
     if (called) {
-      return value
+      return value;
     }
 
-    value = fn.apply(this, arguments)
-    called = true
-    return value
-  }
+    value = fn.apply(this, arguments);
+    called = true;
+    return value;
+  };
 }
 
 /**
@@ -53,56 +53,66 @@ function _once (fn) {
  * @param {*} options the given options for the Redis client.
  * @param {*} callback
  */
-function _createClient (name, options, callback) {
-  const log = logger.child({ redis: name })
-  log.debug('Redis creating client: ' + name)
-  let isReady = false
+function _createClient(name, options, callback) {
+  const log = logger.child({ redis: name });
+  log.debug("kth-node-redis: Redis creating client: " + name);
+  let isReady = false;
 
-  const config = {}
-  deepAssign(config, _defaults, options)
-  let client = redis.createClient(config)
+  const config = {};
+  deepAssign(config, _defaults, options);
+  let client = redis.createClient(config);
 
-  callback = _once(callback)
-  _clients[ name ] = client
-  _clients[ name ].log = log
+  callback = _once(callback);
+  _clients[name] = client;
+  _clients[name].log = log;
 
-  log.debug({ clients: Object.keys(_clients) }, 'Redis clients')
+  log.debug(
+    { clients: Object.keys(_clients) },
+    "kth-node-redis: Redis clients"
+  );
 
-  client.on('error', function (err) {
-    log.error({ err: err }, 'Redis client error')
-    callback(err)
-  })
+  client.on("error", function(err) {
+    log.error({ err: err }, "kth-node-redis: Redis client error");
+    callback(err);
+  });
 
-  client.on('warning', function (err) {
-    log.warn({ err: err }, 'Redis client warning')
-  })
+  client.on("warning", function(err) {
+    log.warn({ err: err }, "kth-node-redis: Redis client warning");
+  });
 
-  client.on('connect', function () {
-    log.debug('Redis connected: ' + name)
-  })
+  client.on("connect", function() {
+    log.debug("kth-node-redis: Redis connected: " + name);
+  });
 
-  client.on('ready', function () {
-    log.debug('Redis client ready: ' + name)
-    log.debug(`Redis server version: ${safeGet(() => client.server_info.redis_version)}`)
-    isReady = true
-    callback(null, client)
-  })
+  client.on("ready", function() {
+    log.debug("kth-node-redis: Redis client ready: " + name);
+    log.debug(
+      `kth-node-redis: Redis server version: ${safeGet(
+        () => client.server_info.redis_version
+      )}`
+    );
+    isReady = true;
+    callback(null, client);
+  });
 
-  client.on('reconnecting', function () {
-    log.debug('Redis client reconnecting: ' + name)
-  })
+  client.on("reconnecting", function() {
+    log.debug("kth-node-redis: Redis client reconnecting: " + name);
+  });
 
-  client.on('end', function () {
-    log.debug('Redis client end: ' + name)
+  client.on("end", function() {
+    log.debug("kth-node-redis: Redis client end: " + name);
     // Close the connection before removing reference.
-    client.quit()
-    client = null
-    delete _clients[ name ]
-    log.debug({ clients: Object.keys(_clients) }, 'Redis clients: ' + _clients.length)
+    client.quit();
+    client = null;
+    delete _clients[name];
+    log.debug(
+      { clients: Object.keys(_clients) },
+      "kth-node-redis: Redis clients: " + _clients.length
+    );
     if (!isReady) {
-      callback(new Error('Done - Failed to connect to Redis'))
+      callback(new Error("kth-node-redis: Failed to connect to Redis"));
     }
-  })
+  });
 }
 
 /**
@@ -111,78 +121,78 @@ function _createClient (name, options, callback) {
  * @param {Object} options - Redis configuration.
  * @returns {Promise} Promise that resolves to a Redis client.
  */
-module.exports = function (name, options) {
-  name = name || _defaultName
-  let client = _clients[ name ]
+module.exports = function(name, options) {
+  name = name || _defaultName;
+  let client = _clients[name];
 
   if (client) {
-    logger.debug(`Redis using client: ${name}`)
-    return Promise.resolve(client)
+    logger.debug(`kth-node-redis: Redis using client: ${name}`);
+    return Promise.resolve(client);
   }
 
-  return new Promise(function (resolve, reject) {
-    _createClient(name, options, function (err, client) {
+  return new Promise(function(resolve, reject) {
+    _createClient(name, options, function(err, client) {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(client)
+        resolve(client);
       }
-    })
-  })
-}
+    });
+  });
+};
 
 /**
  * Get Redis client based on the given client name.
  * @param {*} name The name of the client to get.
  * @param {*} options given options for Redis client
  */
-module.exports.getClient = function (name, options) {
-  name = name || _defaultName
-  let client = _clients[ name ]
+module.exports.getClient = function(name, options) {
+  name = name || _defaultName;
+  let client = _clients[name];
   if (client) {
-    return client
+    return client;
   }
 
-  const config = {}
-  deepAssign(config, _defaults, options)
-  client = redis.createClient(config)
-  _clients[ name ] = client
+  const config = {};
+  deepAssign(config, _defaults, options);
+  client = redis.createClient(config);
+  _clients[name] = client;
 
-  client.on('end', function () {
-    client = null
-    delete _clients[ name ]
-  })
-  return client
-}
+  client.on("end", function() {
+    client = null;
+    delete _clients[name];
+  });
+  return client;
+};
 
 /**
  * Get a connected Redis client or a Promise if the given client is not connected.
  * @param {*} clientName the name of the client to get.
  * @returns {Promise} Promise that resolves to a Redis client or a Redis client if already connected.
  */
-module.exports.connectClient = function (clientName) {
-  let client = _clients[ clientName ]
-  if (!client) throw new Error('No such client: ' + clientName)
-  if (client.connected) return client // we are connected
+module.exports.connectClient = function(clientName) {
+  let client = _clients[clientName];
+  if (!client) throw new Error("kth-node-redis: No such client: " + clientName);
+  if (client.connected) return client; // we are connected
   return new Promise((resolve, reject) => {
-    client.on('ready', function () {
-      resolve(client)
-    })
-    client.on('error end warning', function (err) {
-      reject(err)
-    })
-  })
-}
+    client.on("ready", function() {
+      resolve(client);
+    });
+    client.on("error end warning", function(err) {
+      reject(err);
+    });
+  });
+};
 
 /**
  * Helper to close a Redis client connection by its name.
  * @param {String} name - Name of the Redis instance.
  */
-module.exports.quit = function (name) {
-  name = name || _defaultName
-  let client = _clients[ name ]
+module.exports.quit = function(name) {
+  name = name || _defaultName;
+  let client = _clients[name];
   if (client) {
     // triggers end event
-    client.quit()
+    client.quit();
   }
-}
+};
