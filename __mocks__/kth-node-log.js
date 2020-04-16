903825aecef864c54478b972ed199da5501d9bee
Alexander Urban <lexa@toxa.de>
2020-04-16 17:54:53 +0200

    v3.0.5: Fixing unhandled exceptions (new retry strategy)

--- __mocks__/kth-node-log.js
+++ __mocks__/kth-node-log.js
@@ -1,10 +1,39 @@
-const mockLogger = {
-  error: jest.fn(),
+/* eslint no-use-before-define: ["error", "nofunc"] */
+
+// @ts-check
+
+const mocks = {
   debug: jest.fn(),
   info: jest.fn(),
   warn: jest.fn(),
+  error: jest.fn(),
+  fatal: jest.fn(),
 }
 
-mockLogger.child = jest.fn(() => mockLogger)
+module.exports = {
+  ...mocks,
+  child: jest.fn(() => mocks),
+  _listAllCalls,
+  _clearAllCalls,
+}
 
-module.exports = mockLogger
+function _listAllCalls() {
+  const allCalls = {}
+  let foundCall = false
+
+  const mockedFunctions = Object.keys(mocks)
+
+  mockedFunctions.forEach(name => {
+    const { calls } = mocks[name].mock
+    if (calls.length > 0) {
+      foundCall = true
+      allCalls[name] = calls
+    }
+  })
+
+  return foundCall ? allCalls : null
+}
+
+function _clearAllCalls() {
+  Object.values(mocks).forEach(func => func.mockClear())
+}
