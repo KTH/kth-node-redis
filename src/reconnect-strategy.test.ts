1eb27eb7a48bfd7182986993851e68cafd96da12
Niklas Sandström <mr.n.sandstrom@gmail.com>
2026-05-07 09:43:37 +0200

    keep trying in reconnect strategy

--- src/reconnect-strategy.test.ts
+++ src/reconnect-strategy.test.ts
@@ -8,12 +8,6 @@ describe('createStrategy', () => {
     cleanup = jest.fn()
   })
 
-  it('should stop retrying after 4 retries', () => {
-    const strategy = createStrategy(cleanup)
-    expect(strategy(4, new Error('Some error'))).toBe(false)
-    expect(cleanup).toHaveBeenCalled()
-  })
-
   it('should not reconnect on SocketTimeoutError', () => {
     const strategy = createStrategy(cleanup)
     const error = new SocketTimeoutError(9001)
@@ -34,6 +28,20 @@ describe('createStrategy', () => {
     expect(strategy(1, new Error('Other error'))).toBe(100)
     expect(strategy(2, new Error('Other error'))).toBe(200)
     expect(strategy(3, new Error('Other error'))).toBe(400)
+    expect(strategy(4, new Error('Other error'))).toBe(800)
+    expect(strategy(5, new Error('Other error'))).toBe(1600)
+    expect(strategy(6, new Error('Other error'))).toBe(3200)
+    expect(cleanup).not.toHaveBeenCalled()
+  })
+
+  it('after increasing delay it stays at 5000 ms', () => {
+    const strategy = createStrategy(cleanup)
+    expect(strategy(7, new Error('Other error'))).toBe(5000)
+    expect(strategy(8, new Error('Other error'))).toBe(5000)
+    expect(strategy(15, new Error('Other error'))).toBe(5000)
+    expect(strategy(100, new Error('Other error'))).toBe(5000)
+    expect(strategy(1_000, new Error('Other error'))).toBe(5000)
+    expect(strategy(1_000_000, new Error('Other error'))).toBe(5000)
     expect(cleanup).not.toHaveBeenCalled()
   })
 })
